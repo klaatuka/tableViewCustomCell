7d294152d27d5dcb4c04a3accc9bce4b630930e7
Karen Uzunyan <klaatuka82@gmail.com>
2023-11-25 17:27:11 +0300

    Initial Commit

---
+++ tableViewCustomCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableViewCustomCell
+//
+//  Created by Карен Узунян on 25.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
